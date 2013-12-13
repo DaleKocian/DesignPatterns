2c59da050cd19a3d7243538c9b6382563e8d34e4
dalekocian <dale.kocian@gmail.com>
2013-12-13 04:00:11 -0600

    add prototype example 1

--- src/creational/prototype/example1/FactoryProto.java
+++ src/creational/prototype/example1/FactoryProto.java
@@ -4,4 +4,57 @@ package creational.prototype.example1;
  * Created by dkocian on 12/13/13.
  */
 public class FactoryProto {
+    interface Xyz {
+        Xyz cloan();
+    }
+
+    static class Tom implements Xyz {
+        public Xyz cloan() {
+            return new Tom();
+        }
+
+        public String toString() {
+            return "ttt";
+        }
+    }
+
+    static class Dick implements Xyz {
+        public Xyz cloan() {
+            return new Dick();
+        }
+
+        public String toString() {
+            return "ddd";
+        }
+    }
+
+    static class Harry implements Xyz {
+        public Xyz cloan() {
+            return new Harry();
+        }
+
+        public String toString() {
+            return "hhh";
+        }
+    }
+
+    static class Factory {
+        private static java.util.Map prototypes = new java.util.HashMap();
+
+        static {
+            prototypes.put("tom", new Tom());
+            prototypes.put("dick", new Dick());
+            prototypes.put("harry", new Harry());
+        }
+
+        public static Xyz makeObject(String s) {
+            return ((Xyz) prototypes.get(s)).cloan();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.print(Factory.makeObject(arg) + "  ");
+        }
+    }
 }
